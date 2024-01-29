c57c829af60b30da11c6bf37a51e571259889cf7
mohd furqan <mohdfurqan@mohds-MacBook-Air.local>
2024-01-29 20:52:58 +0530

    added extent reporting

--- src/test/java/com/demo/testcases/AddCustomerTest.java
+++ src/test/java/com/demo/testcases/AddCustomerTest.java
@@ -1,4 +1,10 @@
 package com.demo.testcases;
 
+import org.testng.annotations.Test;
+
 public class AddCustomerTest {
+   // @Test(dataProvider = "getData")
+  //  public void addCustomer(String firstname,String lastname,String postcode){
+
+    //}
 }
